4364d3c2ad859b75d26ba41806a1381739730b42
Jose Vicente Orts Romero <jvortsromero@gmail.com>
2017-06-09 15:50:00 +0200

    Fixed script

--- create-laravel-dockers.sh
+++ create-laravel-dockers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker rm laravel-nginx -f
-docker build -t josrom/laravel-nginx:v5.4 .
+docker build -t josrom/laravel-nginx-docker:v5.4 .
 docker ps -a
-docker create -it --name laravel-nginx -p 8000:80 -p 44300:443 -p 2222:22 josrom/laravel-nginx:5.4
+docker create -it --name laravel-nginx -p 8000:80 -p 44300:443 -p 2222:22 josrom/laravel-nginx-docker:5.4
 docker start laravel-nginx -i
